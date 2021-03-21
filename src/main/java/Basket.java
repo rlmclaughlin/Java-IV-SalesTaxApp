0f5e84ff0931dd60dfe5f94be4b2c84020325318
rlmclaughlin <rlmclaughlin@yahoo.com>
2021-03-21 04:10:02 -0500

    test passed that accumulates total price of the products list

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Basket {
 
     ArrayList<Product> products = new ArrayList<>();
+    double total = 0.00;
 
     public void addToBasket(Product product){
         products.add(product);
@@ -14,5 +15,12 @@ public class Basket {
 
     public void setProducts(ArrayList<Product> products) {
         this.products = products;
+    }
+
+    public double getTotal(){
+        for(Product index : getProducts()){
+            total += index.getPrice();
+        }
+        return total;
     }
 }
