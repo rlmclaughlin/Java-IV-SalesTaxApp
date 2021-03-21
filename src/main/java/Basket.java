a0255c756d2bee934a56db6a5ee3b7a22dd1f97a
rlmclaughlin <rlmclaughlin@yahoo.com>
2021-03-21 03:38:17 -0500

    failed test that checks basket size to confirm if product was added to basket

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -2,4 +2,10 @@ import java.util.ArrayList;
 
 public class Basket {
 
+    ArrayList<Product>products=new ArrayList<>();
+
+    public void addToBasket(Product product){
+        products.add(product);
+    }
+    
 }
--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -1,12 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasketTest {
 
+    Basket basket = new Basket();
+
     @Test
     public void setup(){
-        Basket basket = new Basket();
     }
-    
+
+    @Test
+    public void basketIsCreated(){
+        Product chocolateBar = new Product("Chocolate Bar", 12.49, 0.00, false);
+        basket.addToBasket(chocolateBar);
+        int actual = 0;
+        int expected = basket.products.size();
+        assertEquals(expected, actual);
+    }
+
 }
