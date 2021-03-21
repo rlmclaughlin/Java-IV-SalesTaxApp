9b0105319bba89b71bdec179eb1238aa5560f8de
rlmclaughlin <rlmclaughlin@yahoo.com>
2021-03-21 03:40:42 -0500

    passed test that checks size of basket to confirm product was added

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -7,5 +7,5 @@ public class Basket {
     public void addToBasket(Product product){
         products.add(product);
     }
-    
+
 }
--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -10,13 +10,14 @@ class BasketTest {
 
     @Test
     public void setup(){
+
     }
 
     @Test
-    public void basketIsCreated(){
+    public void itemAddedToBasket(){
         Product chocolateBar = new Product("Chocolate Bar", 12.49, 0.00, false);
         basket.addToBasket(chocolateBar);
-        int actual = 0;
+        int actual = 1;
         int expected = basket.products.size();
         assertEquals(expected, actual);
     }
